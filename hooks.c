16e1724be35e8fcba0e986245a5a52fd08ec4062
Eugene Molotov <em92@users.noreply.github.com>
2016-12-15 06:49:16 +0300

    fixed vm_call_table offset

--- hooks.c
+++ hooks.c
@@ -49,6 +49,7 @@ void __cdecl My_Sys_SetModuleOffset(char* moduleName, void* offset) {
     else
         DebugPrint("Unknown module: %s\n", moduleName);
     
+    printf("without this printf will be segfault\n");
     Sys_SetModuleOffset(moduleName, offset);
     if (common_initialized) {
     	SearchVmFunctions();
@@ -302,7 +303,7 @@ void HookVm(void) {
 #if defined(__x86_64__) || defined(_M_X64)
     pint vm_call_table = *(int32_t*)OFFSET_RELP_VM_CALL_TABLE + OFFSET_RELP_VM_CALL_TABLE + 4;
 #elif defined(__i386) || defined(_M_IX86)
-    pint vm_call_table = *(int32_t*)OFFSET_RELP_VM_CALL_TABLE + 0xCAFF4 + (pint)qagame;
+    pint vm_call_table = *(int32_t*)OFFSET_RELP_VM_CALL_TABLE + 0xCF940 + (pint)qagame;
 #endif
 
 	G_InitGame = *(G_InitGame_ptr*)(vm_call_table + RELOFFSET_VM_CALL_INITGAME);
@@ -320,11 +321,13 @@ void HookVm(void) {
 		failed = 1;
 	}
 
+/*
     res = Hook((void*)G_StartKamikaze, My_G_StartKamikaze, (void*)&G_StartKamikaze);
     if (res) {
         DebugPrint("ERROR: Failed to hook G_StartKamikaze: %d\n", res);
         failed = 1;
     }
+*/
 
     res = Hook((void*)ClientSpawn, My_ClientSpawn, (void*)&ClientSpawn);
     if (res) {
